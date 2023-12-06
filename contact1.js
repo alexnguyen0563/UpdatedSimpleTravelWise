751a03681ffa0c93d5faae73775652c6aed90170
harshverma23 <136378042+harshverma23@users.noreply.github.com>
2023-12-06 13:57:23 -0800

    Update contact1.js

--- contact1.js
+++ contact1.js
@@ -15,16 +15,16 @@ detailsRef.on("child_added", function(snapshot, prevChildKey) {
     var newPost = snapshot.val();
 });
 function send(){
-    var Full_Name = document.getElementById("Full_Name").value;
-    var Email = document.getElementById("Email").value;
-    var Subject = document.getElementById("Subject").value;
-    var Your_Message = document.getElementById("Your_Message").value;
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
 
     detailsRef.push().set({
-        Full_Name: Full_Name: ,
-        Email: Email,
-        Subject: Subject,
-        Your_Message: Your_Message
+        name: name: ,
+        email: email,
+        subject: subject,
+        message: message
 
     });
 }
